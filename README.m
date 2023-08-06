df6f05881bf0ea1c22bbc0829f02607d71736ed1
awomae <141563591+awomae@users.noreply.github.com>
2023-08-06 14:21:36 +0100

    I'm open to new knowledge
    I'm new to this and will appreciate any form of help in getting better at coding and other interesting digital skill

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi I’m @awomae i'm a beginner 
+- 👀 I’m interested in coding, digital marketing/e-commerce..
+- 🌱 I’m currently learning coding ...
+- 💞️ I’m looking to collaborate on all of the above...
+- 📫 How to reach me whatsapp +2348131878264..
+
+<!---
+awomae/awomae is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
